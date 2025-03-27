6cb916214e7f5e39d3b2265da482b9512a4849cb
ethansolnik <ethansolnik@gmail.com>
2025-03-27 03:39:42 -0400

    usecli

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+'use client';
+
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
 // Remove: import Blog from './components/homepage/blog';
